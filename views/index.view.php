246fc80f70c2da886454fd603cad673b86d3e021
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-05-08 16:49:09 +0200

    refactor: move the scripts file in the bottom of the file

--- views/index.view.php
+++ views/index.view.php
@@ -5,11 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Expense Tracker</title>
+    <!-- TAILWIND CDN-->
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <script src="js/functions.js"></script>
-    <script src="js/javascript-fundamentals.js"></script>
-    <script src="js/conditions.js"></script>
-    <script src="js/arrays.js"></script>
 </head>
 
 <body class="mx-auto h-full max-w-7xl">
@@ -55,6 +52,10 @@
         <ul id="error-list"></ul>
     </div>
 </div>
+<script src="js/functions.js"></script>
+<script src="js/javascript-fundamentals.js"></script>
+<script src="js/conditions.js"></script>
+<script src="js/arrays.js"></script>
 </body>
 
 </html>
